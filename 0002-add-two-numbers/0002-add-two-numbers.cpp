e82fdf2e01e7352088f3513dd159778ec3c3188b
Ravi Ranjan <137782361+raviranjancom@users.noreply.github.com>
2024-10-07 21:44:08 +0530

    Time: 7 ms (99.47%), Space: 77.9 MB (6.5%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,28 +11,34 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != NULL || l2 != NULL || carry != 0) {
-            int digit1 = (l1 != NULL) ? l1->val : 0;
-            int digit2 = (l2 != NULL) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != NULL) ? l1->next : NULL;
-            l2 = (l2 != NULL) ? l2->next : NULL;
+        ListNode *p=l1,*q=l2;
+        int c=0;
+        vector<int> v;
+        while(l1!=NULL || l2!=NULL){    // int digit2 = (l2 != NULL) ? l2->val : 0;
+            int d1 = (l1 != NULL) ? l1->val : 0;
+            int d2=(l2!= NULL) ? l2->val : 0;
+            int result = d1 + d2 + c;
+            c = result / 10;
+            v.push_back(result % 10);
+            l1 ? l1=l1->next : NULL;
+            l2 ? l2=l2->next : NULL;
         }
+        if(c!=0) v.push_back(c);
 
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+        ListNode *ans,*last;
+        ans=new ListNode;
+        ans->val=v[0];
+        ans->next=NULL;
+        last=ans;
+        int i=1;
+        while(i<v.size()){
+            ListNode * temp;
+            temp=new ListNode;
+            temp->val=v[i];
+            last->next=temp;
+            last=temp;
+            i++;
+        }
+        return ans;
     }
 };
