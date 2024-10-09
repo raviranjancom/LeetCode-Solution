98a3292d627da277b1b2f638fd89641aa340756c
Ravi Ranjan <137782361+raviranjancom@users.noreply.github.com>
2024-10-09 23:14:57 +0530

    Time: 3 ms (79.65%), Space: 12.6 MB (81.04%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,23 @@
+void fun(vector<int> &cand, vector<vector<int>> &ans, vector<int> &ds, int target, int i){
+    if(i>=cand.size()){
+        if(target==0){
+            ans.push_back(ds);
+        }
+        return ;
+    }
+    if(cand[i]<=target){
+        ds.push_back(cand[i]);
+        fun(cand, ans, ds, target-cand[i], i);
+        ds.pop_back();
+    }
+    fun(cand, ans, ds, target, i+1);
+}
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<vector<int>>> dp(target + 1);
-        dp[0].push_back({});
-
-        for (int i = 1; i <= target; i++) {
-            for (int candidate : candidates) {
-                if (candidate <= i) {
-                    for (auto& prev : dp[i - candidate]) {
-                        vector<int> temp(prev);
-                        temp.push_back(candidate);
-                        sort(temp.begin(), temp.end());
-                        if (std::find(dp[i].begin(), dp[i].end(), temp) ==
-                            dp[i].end()) {
-                            dp[i].push_back(temp);
-                        }
-                    }
-                }
-            }
-        }
-        return dp[target];
+        vector<vector<int>> ans;
+        vector<int> ds;
+        fun(candidates, ans, ds, target, 0);
+        return ans;
     }
 };
